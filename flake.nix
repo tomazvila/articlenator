95c46d99861942920a428dfbb22a25b6fc4be91f
tomazvila <tomazvila@outlook.com>
2026-01-08 20:27:23 +0200

    test CI stuff 3

--- flake.nix
+++ flake.nix
@@ -20,6 +20,8 @@
       dockerSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       forDockerSystems = nixpkgs.lib.genAttrs dockerSystems;
+      # Helper to check if system supports Docker
+      isDockerSystem = system: builtins.elem system dockerSystems;
     in {
       packages = forAllSystems (system:
         let
@@ -56,148 +58,117 @@
           );
 
           venv = pythonSet.mkVirtualEnv "twitter-articlenator-env" workspace.deps.default;
-        in {
-          default = venv;
-        }
-      ) // (forDockerSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          python = pkgs.python314;
 
-          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-          overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
-
-          pyprojectOverrides = final: prev: {
-            weasyprint = prev.weasyprint.overrideAttrs (old: {
-              nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
-                pkgs.pkg-config
-              ];
-              buildInputs = (old.buildInputs or []) ++ [
+          # Docker-specific definitions (only for Linux)
+          dockerPkgs = if isDockerSystem system then
+            let
+              # Runtime dependencies for the container
+              runtimeDeps = [
                 pkgs.pango
                 pkgs.cairo
                 pkgs.gdk-pixbuf
                 pkgs.fontconfig
+                pkgs.glib
+                pkgs.harfbuzz
+                pkgs.freetype
+                pkgs.zlib
                 pkgs.gobject-introspection
+                # Fonts for PDF rendering
+                pkgs.dejavu_fonts
+                pkgs.liberation_ttf
+                pkgs.noto-fonts
+                # Playwright/Chromium dependencies
+                pkgs.playwright-driver.browsers
+                pkgs.chromium
+                pkgs.nss
+                pkgs.nspr
+                pkgs.atk
+                pkgs.cups
+                pkgs.libdrm
+                pkgs.gtk3
+                pkgs.alsa-lib
+                pkgs.at-spi2-atk
+                pkgs.at-spi2-core
+                pkgs.libxkbcommon
+                pkgs.xorg.libX11
+                pkgs.xorg.libXcomposite
+                pkgs.xorg.libXdamage
+                pkgs.xorg.libXext
+                pkgs.xorg.libXfixes
+                pkgs.xorg.libXrandr
+                pkgs.mesa
+                pkgs.expat
+                pkgs.dbus
+                # Basic utilities
+                pkgs.coreutils
+                pkgs.bash
+                pkgs.cacert
               ];
-            });
-          };
 
-          pythonSet = (pkgs.callPackage pyproject-nix.build.packages {
-            inherit python;
-          }).overrideScope (
-            pkgs.lib.composeManyExtensions [
-              pyproject-build-systems.overlays.default
-              overlay
-              pyprojectOverrides
-            ]
-          );
+              # Create fontconfig cache
+              fontsConf = pkgs.makeFontsConf {
+                fontDirectories = [
+                  pkgs.dejavu_fonts
+                  pkgs.liberation_ttf
+                  pkgs.noto-fonts
+                ];
+              };
 
-          venv = pythonSet.mkVirtualEnv "twitter-articlenator-env" workspace.deps.default;
+              # Wrapper script to run the app
+              entrypoint = pkgs.writeShellScriptBin "entrypoint" ''
+                export FONTCONFIG_FILE="${fontsConf}"
+                export GI_TYPELIB_PATH="${pkgs.lib.makeSearchPath "lib/girepository-1.0" [
+                  pkgs.pango pkgs.gdk-pixbuf pkgs.gobject-introspection
+                ]}"
+                export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeDeps}"
+                export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
+                export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+                export HOME="/tmp"
+                export XDG_CONFIG_HOME="/data/config"
+                export TWITTER_ARTICLENATOR_CONFIG_DIR="/data/config"
+                export TWITTER_ARTICLENATOR_OUTPUT_DIR="/data/output"
 
-          # Runtime dependencies for the container
-          runtimeDeps = [
-            pkgs.pango
-            pkgs.cairo
-            pkgs.gdk-pixbuf
-            pkgs.fontconfig
-            pkgs.glib
-            pkgs.harfbuzz
-            pkgs.freetype
-            pkgs.zlib
-            pkgs.gobject-introspection
-            # Fonts for PDF rendering
-            pkgs.dejavu_fonts
-            pkgs.liberation_ttf
-            pkgs.noto-fonts
-            # Playwright/Chromium dependencies
-            pkgs.playwright-driver.browsers
-            pkgs.chromium
-            pkgs.nss
-            pkgs.nspr
-            pkgs.atk
-            pkgs.cups
-            pkgs.libdrm
-            pkgs.gtk3
-            pkgs.alsa-lib
-            pkgs.at-spi2-atk
-            pkgs.at-spi2-core
-            pkgs.libxkbcommon
-            pkgs.xorg.libX11
-            pkgs.xorg.libXcomposite
-            pkgs.xorg.libXdamage
-            pkgs.xorg.libXext
-            pkgs.xorg.libXfixes
-            pkgs.xorg.libXrandr
-            pkgs.mesa
-            pkgs.expat
-            pkgs.dbus
-            # Basic utilities
-            pkgs.coreutils
-            pkgs.bash
-            pkgs.cacert
-          ];
+                exec ${venv}/bin/twitter-articlenator "$@"
+              '';
+            in {
+              docker = pkgs.dockerTools.buildLayeredImage {
+                name = "twitter-articlenator";
+                tag = "latest";
 
-          # Create fontconfig cache
-          fontsConf = pkgs.makeFontsConf {
-            fontDirectories = [
-              pkgs.dejavu_fonts
-              pkgs.liberation_ttf
-              pkgs.noto-fonts
-            ];
-          };
+                contents = [
+                  venv
+                  entrypoint
+                ] ++ runtimeDeps;
 
-          # Wrapper script to run the app
-          entrypoint = pkgs.writeShellScriptBin "entrypoint" ''
-            export FONTCONFIG_FILE="${fontsConf}"
-            export GI_TYPELIB_PATH="${pkgs.lib.makeSearchPath "lib/girepository-1.0" [
-              pkgs.pango pkgs.gdk-pixbuf pkgs.gobject-introspection
-            ]}"
-            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeDeps}"
-            export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
-            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-            export HOME="/tmp"
-            export XDG_CONFIG_HOME="/data/config"
-            export TWITTER_ARTICLENATOR_CONFIG_DIR="/data/config"
-            export TWITTER_ARTICLENATOR_OUTPUT_DIR="/data/output"
+                extraCommands = ''
+                  mkdir -p data/config data/output tmp
+                '';
 
-            exec ${venv}/bin/twitter-articlenator "$@"
-          '';
+                config = {
+                  Entrypoint = [ "${entrypoint}/bin/entrypoint" ];
+                  Env = [
+                    "PYTHONUNBUFFERED=1"
+                    "TWITTER_ARTICLENATOR_JSON_LOGGING=true"
+                  ];
+                  ExposedPorts = {
+                    "5001/tcp" = {};
+                  };
+                  Volumes = {
+                    "/data" = {};
+                  };
+                  WorkingDir = "/";
+                  Labels = {
+                    "org.opencontainers.image.source" = "https://github.com/user/twitter-articlenator";
+                    "org.opencontainers.image.description" = "Twitter Article to PDF Converter";
+                  };
+                };
+              };
+            }
+          else {};
         in {
-          # Docker image for deployment
-          docker = pkgs.dockerTools.buildLayeredImage {
-            name = "twitter-articlenator";
-            tag = "latest";
-
-            contents = [
-              venv
-              entrypoint
-            ] ++ runtimeDeps;
-
-            extraCommands = ''
-              mkdir -p data/config data/output tmp
-            '';
-
-            config = {
-              Entrypoint = [ "${entrypoint}/bin/entrypoint" ];
-              Env = [
-                "PYTHONUNBUFFERED=1"
-                "TWITTER_ARTICLENATOR_JSON_LOGGING=true"
-              ];
-              ExposedPorts = {
-                "5001/tcp" = {};
-              };
-              Volumes = {
-                "/data" = {};
-              };
-              WorkingDir = "/";
-              Labels = {
-                "org.opencontainers.image.source" = "https://github.com/user/twitter-articlenator";
-                "org.opencontainers.image.description" = "Twitter Article to PDF Converter";
-              };
-            };
-          };
-        }
-      ));
+          default = venv;
+        } // dockerPkgs
+      );
 
       apps = forAllSystems (system: {
         default = {
